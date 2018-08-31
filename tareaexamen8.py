b21c73bd8ded1a166e76c609b69ee7dc25fd6087
javierjuarez7 <42384775+javierjuarez7@users.noreply.github.com>
2018-08-30 22:25:28 -0500

    Add files via upload

---
+++ tareaexamen8.py
@@ -0,0 +1,7 @@
+#Javier Juarez Jimenez
+#Programa que calcula el area de un triangulo
+
+b=float(input('Introduce la base del triangulo: '))
+h=float(input('Introduce la altura del triangulo: '))
+A=0.5*b*h
+print(A)
